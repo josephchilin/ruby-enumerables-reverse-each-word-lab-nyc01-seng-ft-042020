e1c5894d29b8dc36cc239d0f6d22d289e9c5eb4f
Jo L <joseph@josephchilin.com>
2020-04-16 02:42:48 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,5 +4,5 @@ def reverse_each_word(sentence)
   sentence_array = sentence.split.collect do |sentence_reverse|
   sentence_reverse.reverse
   end
-  sentence_array.join
+  sentence_array.join(" ")
 end
